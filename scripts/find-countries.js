b8e9b0ee71b561aadd092dc678492cea25c822f3
russellgoldenberg <russellgoldenberg@gmail.com>
2018-11-07 14:44:42 -0500

    upgrade country finding

--- scripts/find-countries.js
+++ scripts/find-countries.js
@@ -5,23 +5,107 @@ const uniq = require('lodash.uniqby');
 
 mkdirp('./output/months-with-countries');
 
-const countries = d3.csvParse(
-  fs.readFileSync('./output/countries.csv', 'utf-8')
+// remove US
+const countries = d3
+  .csvParse(fs.readFileSync('./output/countries.csv', 'utf-8'))
+  .filter(d => !d.common.includes('United States'));
+
+const renaming = d3.csvParse(
+  fs.readFileSync('./input/geographical_renaming.csv', 'utf-8')
 );
 
-function checkMatch({ c, d, f }) {
-  const common = c.common.toLowerCase();
-  const demonym = c.demonym.toLowerCase();
-  const hasCommon = d.headline.toLowerCase().includes(common);
-  const hasDemonym = d.headline.toLowerCase().includes(demonym);
-  if (f === 'common') return common && hasCommon;
-  else if (f === 'demonym') return demonym && hasDemonym;
-  return (common && hasCommon) || (demonym && hasDemonym);
+// rules
+// common in other demonym: check that other demonym doesn't exist (eg. Georgia and South Georgian)
+// common means other thing https://github.com/the-pudding/nyt-map-data/issues/5
+
+countries.forEach(c => (c.rename = []));
+renaming.forEach(r => {
+  // merge renamings to countries
+  const matchA = countries.find(c => c.common === r.current);
+  const matchB = countries.find(c => c.official === r.current);
+  if (matchA) matchA.rename.push(r.previous.toLowerCase().trim());
+  else if (matchB) matchB.rename.push(r.previous.toLowerCase().trim());
+  else console.log('no renaming match', r);
+});
+
+countries.forEach((country, index) => {
+  // if a denonym is not perfectly unique (eg. Dominican) then we can't use
+  const match = countries.find(
+    (c, i) => index !== i && country.demonym === c.demonym
+  );
+  if (match) country.demonym = '';
+  country.commonLower = country.common.toLowerCase().trim();
+  country.demonymLower = country.demonym.toLowerCase().trim();
+});
+
+countries.forEach((country, index) => {
+  // setup exclusions to check if it finds a match
+  // common in other common
+  const cc = countries.filter(
+    (c, i) => index !== i && c.commonLower.includes(country.commonLower)
+  );
+  // common in other dem
+  const cd = countries.filter(
+    (c, i) => index !== i && c.demonymLower.includes(country.commonLower)
+  );
+  country.commonExclude = [].concat(
+    ...cc.map(c => c.commonLower),
+    ...cd.map(c => c.demonymLower)
+  );
+
+  // dem in other common
+  const dc = country.demonymLower
+    ? countries.filter(
+        (c, i) => index !== i && c.commonLower.includes(country.demonymLower)
+      )
+    : [];
+  // dem in other dem
+  const dd = country.demonymLower
+    ? countries.filter(
+        (c, i) => index !== i && c.demonymLower.includes(country.demonymLower)
+      )
+    : [];
+
+  country.demonymExclude = [].concat(
+    ...dc.map(c => c.commonLower),
+    ...dd.map(c => c.demonymLower)
+  );
+});
+
+function includes({ h, inc, exc }) {
+  const found = inc.find(i => h.includes(i));
+  if (!exc.length) return found;
+  return found && !exc.find(e => h.includes(e));
+}
+
+function checkMatch({ c, h, f }) {
+  if (f === 'common')
+    return includes({
+      h,
+      inc: [c.commonLower, ...c.rename],
+      exc: c.commonExclude
+    });
+  else if (f === 'demonym')
+    return (
+      c.demonymLower &&
+      includes({ h, inc: [c.demonymLower], exc: c.demonymExclude })
+    );
+  return (
+    h.includes({
+      h,
+      inc: [c.commonLower, ...c.rename],
+      exc: c.commonExclude
+    }) ||
+    (c.demonymLower &&
+      includes({ h, inc: [c.demonymLower], exc: c.demonymExclude }))
+  );
 }
 
 function findCountries({ data, year, month }) {
   const filtered = data.filter(d => {
-    const exists = countries.find(c => checkMatch({ c, d, f: 'both' }));
+    const exists = countries.find(c =>
+      checkMatch({ c, h: d.headline.toLowerCase(), f: 'both' })
+    );
     return exists;
   });
   const output = d3.csvFormat(filtered);
@@ -37,7 +121,7 @@ function init() {
   for (f in files) {
     console.log(files[f]);
     const data = d3.csvParse(
-      fs.readFileSync(`./output/months/${files[f]}`, 'utf-8')
+      fs.readFileSync(`./output/months-clean/${files[f]}`, 'utf-8')
     );
     const split = files[f].split('-');
     const year = split[0];
